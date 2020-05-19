0898e58bb2041c34ec1f5e1af963d136309f04ae
hoangalhp123 <hoangnx3@viettelpost.com.vn>
2020-05-19 18:22:33 +0700

    sdfdsf

---
+++ Assets/Script/BgrMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgrMove : MonoBehaviour
+{
+    private GameObject obj;
+    public float speedMove;
+    public float moveRange;
+    private Vector3 oldVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        speedMove = 3;
+        moveRange = 20.5f;
+        oldVector = obj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.Translate(new Vector3(-1 * Time.deltaTime * speedMove, 0, 0));
+        if (Vector3.Distance(oldVector, obj.transform.position) > moveRange) {
+            obj.transform.position = oldVector;
+        }
+    }
+}
---
+++ Assets/Script/WallMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMove : MonoBehaviour
+{
+    private GameObject obj;
+    public float speedMove;
+
+    public float minY;
+    public float maxY;
+    public float oldPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        speedMove = 5;
+        oldPosition = 5;
+        minY = -1;
+        maxY = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.Translate(new Vector3(-1 * Time.deltaTime * speedMove, 0, 0));
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("ResetWall"))
+        {
+            obj.transform.position = new Vector3(oldPosition, Random.Range(minY, maxY + 1), 0);
+        }
+        
+    }
+
+}
